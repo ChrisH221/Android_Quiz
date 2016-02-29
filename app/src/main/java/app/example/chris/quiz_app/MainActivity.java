5a1f9d72cd30f080136a4869bffcbf1ac99c7fd1
Chris <C.Howell2@uni.brighton.ac.uk>
2016-02-29 15:54:26 +0000

    Implemented get JSON array.
    Uses async task to return a JSON array to the main activity.

--- app/src/main/java/app/example/chris/quiz_app/MainActivity.java
+++ app/src/main/java/app/example/chris/quiz_app/MainActivity.java
@@ -1,5 +1,6 @@
 package app.example.chris.quiz_app;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,16 +9,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
-
+    JSONArray arr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbAsync db = new dbAsync();
+        db.execute();
+        final Button buttonS = (Button) findViewById(R.id.button_start);
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView tv = (TextView)findViewById(R.id.textView1);
+                tv.setText(arr.toString());
+            }
+        });
 
-        dbhelper db = new dbhelper();
-        db.query();
     }
 
     @Override
@@ -27,5 +48,61 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void parseQuestion(JSONArray arr){
+
+
+       this.arr=arr;
+
+
+    }
+
+    class dbAsync extends AsyncTask<Void, JSONArray, String> {
+
+
+
+        protected String doInBackground(Void... args) {
+
+            String site = "http://192.168.1.67:80/getquestion.php";
+            try {
+                URL url = new URL(site);
+                URLConnection urlConn = url.openConnection();
+                HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+                httpConn.setRequestMethod("GET");
+                httpConn.connect();
+                httpConn.getResponseCode();
+
+                InputStream input =  httpConn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                StringBuilder result = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+                String response = result.toString();
+
+                return response;
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+                e.printStackTrace();
+                return null;
+
+            }}
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                JSONArray res = new JSONArray(result);
+                parseQuestion(res);
+                          } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+    }
 
 }
