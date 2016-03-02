1ee4ac266e10d689ea50db91eacb6d8b1e73b619
Chris <C.Howell2@uni.brighton.ac.uk>
2016-03-02 17:10:50 +0000

    Finished countdown.

--- app/src/main/java/app/example/chris/quiz_app/MainActivity.java
+++ app/src/main/java/app/example/chris/quiz_app/MainActivity.java
@@ -23,22 +23,13 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     JSONArray arr;
-    int score;
+    final int score =0;
     double time;
     boolean finish = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        JSONAsync db = new JSONAsync();
-        db.execute();
-        final Button buttonS = (Button) findViewById(R.id.button_start);
-        buttonS.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                gameLoop(arr, 0, score = 0);
-                time();
-               }
-        });
+        setupMain();
 
     }
 
@@ -48,42 +39,89 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    public void setupMain(){
+        setContentView(R.layout.activity_main);
+        JSONAsync db = new JSONAsync();
+        db.execute();
+        httpPost post = new httpPost(10, "Chris");
+        post.execute();
 
-    public void parseQuestion(JSONArray arr){ this.arr=arr; }
+        final Button buttonS = (Button) findViewById(R.id.button_start);
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main2);
+                gameLoop(arr, 0, score);
+                time();
+            }
+        });
+
+
+    }
+
+    public void parseQuestion(JSONArray arr){ this.arr = null; this.arr=arr; }
 
     public void time(){
 
-
         final TextView timer = (TextView) findViewById( R.id.timer );
-        new CountDownTimer(30000, 1000) { // adjust the milli seconds here
+        new CountDownTimer(10000, 1000) { // adjust the milli seconds here
 
             public void onTick(long millisUntilFinished) {
 
-                timer.setText(""+String.format("%d min, %d sec",
-                        TimeUnit.MILLISECONDS.toMinutes( millisUntilFinished),
-                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
-                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                timer.setText("" + String.format("%d min, %d sec",TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                              TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
 
 
             }
 
             public void onFinish() {
-                finish = true;
+                finish(score);
             }
         }.start();
 
     }
 
+    public void finish(int score){
+
+        setContentView(R.layout.finish);
+
+        TextView st = (TextView)findViewById(R.id.score_text);
+        st.setText("For this round you scored:" + score);
+        JSONAsync db = new JSONAsync();
+        db.execute();
+
+        final Button buttonT = (Button) findViewById(R.id.button_again);
+        buttonT.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            gameLoop(arr,0,0);
+            }
+        });
+
+
+        final Button buttonM = (Button) findViewById(R.id.button_main);
+        buttonM.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+               setupMain();
+
+            }
+        });
+
+
+    }
+
+
     public int gameLoop(final JSONArray arr, final int count, final int score){
 
 
-        if(finish == true) return score;
+        if(finish == true){return 0;}// finish(score);
+
         final String answer;
 
-        setContentView(R.layout.activity_main2);
+
         TextView tv = (TextView)findViewById(R.id.textView1);
         TextView s = (TextView)findViewById(R.id.score);
-        s.setText("SCORE:"+score);
+        s.setText("SCORE:" + score);
         try {
             JSONObject json_obj = arr.getJSONObject(count);
             String question = json_obj.getString("question");
@@ -94,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
             throw new RuntimeException(e);
         }
 
-        final Button buttonT = (Button) findViewById(R.id.button_true);
+        final Button buttonT = (Button) findViewById(R.id.button_again);
         buttonT.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -111,8 +149,8 @@ public class MainActivity extends AppCompatActivity {
                 else gameLoop(arr,count,score);
             }
         });
+    return 0;
 
-        return score;
     }
 
     class JSONAsync extends AsyncTask<Void, JSONArray, String> {
---
+++ app/src/main/java/app/example/chris/quiz_app/httpPost.java
@@ -0,0 +1,65 @@
+package app.example.chris.quiz_app;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Chris on 02/03/2016.
+ */
+class httpPost extends AsyncTask<Void, Void, String> {
+
+    int score;
+    String playerName;
+
+    public httpPost(int score, String playerName){
+
+
+
+
+    }
+
+    protected String doInBackground(Void... args) {
+
+        String site = "http://52.18.108.189/addUser.php";
+        try {
+            URL url = new URL(site);
+            URLConnection urlConn = url.openConnection();
+            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+            httpConn.setDoOutput(true);
+            OutputStream os = httpConn.getOutputStream();
+            String POST_PARAMS = "score=10@player=chris";
+            System.out.println(POST_PARAMS);
+            os.write(POST_PARAMS.getBytes());
+            int responseCode = httpConn.getResponseCode();
+            httpConn.connect();
+            return "true";
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            e.printStackTrace();
+            return"false";
+
+        }}
+
+    //@Override
+   // protected void onPostExecute(String result) {
+
+     //   try {
+
+     //   } catch (JSONException e) {
+    //        throw new RuntimeException(e);
+     //   }
+
+
+  //  }
+
+}
